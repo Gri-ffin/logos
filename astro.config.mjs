ad8c78321e5b9edc9631f7dd01588e61add82dda
Gri-ffin <gameshunter359@gmail.com>
2025-10-08 14:18:16 +0100

    Add @astrojs/node integration and update configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,19 @@ import react from '@astrojs/react'
 
 import mdx from '@astrojs/mdx'
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [react(), mdx()],
   site: 'https://logos.github.io',
-  base: '/logos'
+  base: '/logos',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 })
